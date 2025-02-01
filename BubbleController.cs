d8cf72a55bf1072f12b3a809486f6d259b51361c
Bastien Gounon <bastien.gounon@novainsilico.ai>
2025-02-01 19:22:03 +0100

    fix: better merging

--- BubbleController.cs
+++ BubbleController.cs
@@ -6,6 +6,8 @@ public class BubbleController : MonoBehaviour
     private float velocity = 1f;
     public float maxVelocity = 15f;
     public float minVelocity = 1f;
+    public Vector3 minScale = new(0.25f, 0.25f, 0.25f);
+    public Vector3 maxScale = new(2.5f, 2.5f, 2.5f);
     private PlayerController currentOwner = null;
     private bool merging = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -47,12 +49,27 @@ public class BubbleController : MonoBehaviour
         velocity = (newVelocity < minVelocity) ? minVelocity : newVelocity;
     }
 
+    public void IncreaseScale(Vector3 delta){
+        Vector3 newScale = GetScale() + delta;
+        if (newScale.x > maxScale.x || newScale.y > maxScale.y || newScale.z > maxScale.z) {
+            newScale = maxScale;
+        }
+        SetScale(newScale);
+    }
+
+    public void ReduceScale(Vector3 delta){
+        Vector3 newScale = GetScale() - delta;
+        if (newScale.x < maxScale.x || newScale.y < maxScale.y || newScale.z < maxScale.z) {
+            newScale = minScale;
+        }
+        SetScale(newScale);
+    }
+
     public void HitBall(PlayerController player, Vector2 direction){
         SwitchOwner(player);
-        Rigidbody ballRb = GetComponent<Rigidbody>();
-        ballRb.linearVelocity = Vector2.zero;
+        GetRigidBody().linearVelocity = Vector2.zero;
         IncreaseVelocity();
-        ballRb.AddForce(direction * velocity, ForceMode.VelocityChange);
+        GetRigidBody().AddForce(direction * velocity, ForceMode.VelocityChange);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -69,20 +86,31 @@ public class BubbleController : MonoBehaviour
     void MergeWith(BubbleController otherBall){
         // Only merge if not already merging
         if (otherBall.isMerging() || isMerging()) return;
-        TriggerMerging();
-        otherBall.TriggerMerging();
-        Rigidbody ballRb = GetComponent<Rigidbody>();
-        ballRb.linearVelocity += otherBall.GetComponent<Rigidbody>().linearVelocity;
-        ballRb.transform.parent.gameObject.transform.localScale += otherBall.transform.parent.gameObject.transform.localScale;
+        SetMerging();
+        otherBall.SetMerging();
+        IncreaseScale(otherBall.GetScale());
         IncreaseVelocity(otherBall.GetVelocity());
         Destroy(otherBall.transform.parent.gameObject);
-        merging = false;
+        UnsetMerging();
     }
-    public void TriggerMerging(){
+    public void SetMerging(){
         merging = true;
+    }
+    public void UnsetMerging(){
+        merging = false;
     }
 
     public bool isMerging(){
         return merging;
+    }
+
+    public Vector3 GetScale(){
+        return GetRigidBody().transform.parent.gameObject.transform.localScale;
+    }
+    public void SetScale(Vector3 scale){
+        GetRigidBody().transform.parent.gameObject.transform.localScale = scale;
+    }
+    private Rigidbody GetRigidBody() {
+        return GetComponent<Rigidbody>();
     }
 }
